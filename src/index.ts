82c2dcc32ba25d1a605fed91773e25b275b94768
Nadelopo <mistrmorsikk@mail.ru>
2022-11-11 17:45:59 +0400

    update

--- src/index.ts
+++ src/index.ts
@@ -6,43 +6,45 @@ GM_addStyle(S)
 const func = () => {
   let interval = 0
 
-  const setBoderAndText = () => {
-    const chatMessage = document.querySelectorAll('.chat-line__no-background')
-    chatMessage.forEach((el) => {
-      const nick = el.querySelector('.chat-author__display-name')
-      let color: string | null = null
-      if (nick) color = window.getComputedStyle(nick).color
+  const setBoderAndText = (el: Element) => {
+    const nick = el.querySelector('.chat-author__display-name')
+    let color: string | null = null
+    if (nick) color = window.getComputedStyle(nick).color
 
-      let border: HTMLElement | null = el.querySelector(
-        '.chat-line__username-container'
-      )
-      if (border && window.getComputedStyle(border).borderColor !== color) {
-        if (color) border.style.borderColor = color
-      }
+    let border: HTMLElement | null = el.querySelector(
+      '.chat-line__username-container'
+    )
+    if (border && window.getComputedStyle(border).borderColor !== color) {
+      if (color) border.style.borderColor = color
+    }
 
-      let text
-      setTimeout(() => {
-        text = el.lastElementChild
-        if (text && window.getComputedStyle(text).color !== color) {
-          if (color) text.style.color = color
-        }
-      })
-
-      let mention: HTMLElement | null = el.querySelector('.seventv-mention')
-      if (!mention) mention = el.querySelector('.mention-fragment')
-      if (mention && window.getComputedStyle(mention).color !== color) {
-        if (color) mention.style.color = color
-      }
-
-      if (interval) {
-        setTimeout(() => {
-          if (text && window.getComputedStyle(text).color === color) {
-            clearInterval(interval)
-            interval = 0
-          }
-        }, 5000)
+    let text
+    setTimeout(() => {
+      text = el.lastElementChild
+      if (text && window.getComputedStyle(text).color !== color) {
+        if (color) text.style.color = color
       }
     })
+
+    let mention: HTMLElement | null = el.querySelector('.seventv-mention')
+    if (!mention) mention = el.querySelector('.mention-fragment')
+    if (mention && window.getComputedStyle(mention).color !== color) {
+      if (color) mention.style.color = color
+    }
+
+    if (interval) {
+      setTimeout(() => {
+        if (text && window.getComputedStyle(text).color === color) {
+          clearInterval(interval)
+          interval = 0
+        }
+      }, 5000)
+    }
+  }
+
+  const setStyles = () => {
+    const chatMessage = document.querySelectorAll('.chat-line__no-background')
+    chatMessage.forEach((el) => setBoderAndText(el))
   }
 
   let lastLocation = ''
@@ -53,8 +55,8 @@ const func = () => {
       let chatContainer = document.querySelector(
         '.chat-scrollable-area__message-container'
       )
-      let chatObserv = new MutationObserver(() => setBoderAndText())
-      interval = setInterval(() => setBoderAndText(), 500)
+      let chatObserv = new MutationObserver(() => setStyles())
+      interval = setInterval(() => setStyles(), 500)
 
       chatObserv.observe(chatContainer, {
         childList: true,
@@ -65,7 +67,8 @@ const func = () => {
   addEventListener('click', () => {
     const open = document.querySelector('.chat-input-tray__open')
     if (open) {
-      setBoderAndText()
+      const message = open.querySelector('.chat-line__no-background')
+      setBoderAndText(message)
     }
   })
 }
